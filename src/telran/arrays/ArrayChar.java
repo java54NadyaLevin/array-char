3a27115f57cd386c4dd01bf11ba5277e9a4cd606
Nadelele <nadele@gmail.com>
2024-03-07 21:04:38 +0200

    classwork completed

--- src/telran/arrays/ArrayChar.java
+++ src/telran/arrays/ArrayChar.java
@@ -2,14 +2,36 @@ package telran.arrays;
 
 
 public class ArrayChar {
-	private char[] array;
+	private char[] array; //encapsulated state
 	public ArrayChar(char[] array) { //class constructor
 		this.array = array; //this - to avoid conflict
 	}
 //massive should be immutable => there's no getter.
 	
 	public int compareTo(ArrayChar another) {
-		//TODO
-		return -1;
+		int length = Math.min(this.array.length, another.array.length);
+		int index = 0;
+		while(index < length && this.array[index] == another.array[index]) {
+			index++;
+		}
+		return index == length ? this.array.length - another.array.length : this.array[index]-another.array[index];
 	}
+	public int indexOf(char character) {
+		int index = 0;
+		while(index < array.length && array[index] != character) {
+			index++;
+		}
+	
+	return index == array.length ? -1 : index;	
+	}
+	
+	public int lastIndexOf(char character) {
+		int index = array.length-1;
+		while(index > -1 && array[index] != character) {
+			index--;
+		}
+		return index;
+	}
+	
+	//compareToIgnoreCase, contains, equals, equalsIgnoreCase (will use compareToIgnoreCase), count which count number of characters are in massive(one character in)
 }
--- src/telran/arrays/test/arrayCharTest.java
+++ src/telran/arrays/test/arrayCharTest.java
@@ -20,9 +20,27 @@ class arrayCharTest {
 		assertEquals(-8, arrayChar1.compareTo(arrayChar2));
 		assertEquals(1, new ArrayChar(new char[] {'a', 'a'})
 				.compareTo(new ArrayChar(new char[] {'a'})));
-		assertEquals(-11, new ArrayChar(new char[] {'a'})
+		assertEquals(-1, new ArrayChar(new char[] {'a'})
 				.compareTo(new ArrayChar(new char[] {'a', 'a'})));
 		assertEquals(0, arrayChar1.compareTo(arrayChar3));
+	}
+	
+	@Test
+	void indexOfTest() {
+		char[] ar1 = {'1', '2', '3', '2'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertEquals(1, arrayChar1.indexOf('2'));
+		assertEquals(-1, arrayChar1.indexOf('4'));
+	
+	}
+	
+	@Test
+	void lastIndexOfTest() {
+		char[] ar1 = {'1', '2', '3', '2'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertEquals(3, arrayChar1.lastIndexOf('2'));
+		assertEquals(-1, arrayChar1.lastIndexOf('4'));
+	
 	}
 
 }
